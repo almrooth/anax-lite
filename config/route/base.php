9474a6377aa59c26e90eae94cb8e5ac9477db8d9
Tobias Almroth <tobalm.vim@gmail.com>
2017-04-06 15:29:54 +0200

    Bare framework built

---
+++ config/route/base.php
@@ -0,0 +1,28 @@
+<?php
+$app->router->add("", function () use ($app) {
+    $app->view->add("talm/header", ["title" => "Home"]);
+    $app->view->add("talm/navbar");
+    $app->view->add("talm/home");
+
+    $app->response->setBody([$app->view, "render"])->send();
+});
+
+$app->router->add("about", function () use ($app) {
+    $app->view->add("talm/header", ["title" => "About"]);
+    $app->view->add("talm/navbar");
+    $app->view->add("talm/about");
+
+    $app->response->setBody([$app->view, "render"])->send();
+});
+
+$app->router->add("status", function () use ($app) {
+    $data = [
+        "Server" => php_uname(),
+        "PHP version" => phpversion(),
+        "Included files" => count(get_included_files()),
+        "Memory used" => memory_get_peak_usage(true),
+        "Execution time" => microtime(true) - $_SERVER["REQUEST_TIME_FLOAT"],
+    ];
+
+    $app->response->sendJson($data);
+});
---
+++ src/App/App.php
@@ -0,0 +1,11 @@
+<?php 
+
+namespace Talm\App;
+
+/**
+ * An App class to wrap the resources of the framework.
+ */
+class App
+{
+    
+}
